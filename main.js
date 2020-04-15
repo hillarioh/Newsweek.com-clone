c81304e079949b3d12479758385b2c97586824c8
hillarioh <okerioh@gmail.com>
2020-04-15 23:05:56 +0300

    Navbar extends full width

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
       $(".head1").addClass("d-none").delay(2500).show();
       $(".head2").removeClass("d-none").delay(2500).show();
       $(".head3").addClass("d-none").delay(2500).show();
+      // $(".naty").removeClass("px-3");
+      // $(".naty").removeClass("px-0");
     } else {
       $(".head2").addClass("d-none").delay(2500).show();
       $(".head1").removeClass("d-none").delay(2500).show();
